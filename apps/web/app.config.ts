60be46aa02995a126dba9bc111ddf6d19a3576bf
Harry Digos <harrydigos@gmail.com>
2025-02-11 20:02:33 +0200

    feat: vite 6 support

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -2,8 +2,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from '@solidjs/start/config';
 
 export default defineConfig({
-  server: {
-    compatibilityDate: '2025-02-08',
-  },
   vite: { plugins: [tailwindcss()] },
 });
