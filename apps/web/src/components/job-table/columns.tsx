cd65dfd9eb3593b17fd179f6c17bcaf2bae7fed6
Harry Digos <harrydigos@gmail.com>
2025-03-16 18:35:35 +0200

    feat(web): enhance tooltip rendering

--- apps/web/src/components/job-table/columns.tsx
+++ apps/web/src/components/job-table/columns.tsx
@@ -1,5 +1,5 @@
-import type { ColumnDef } from '@tanstack/solid-table';
-import { createSignal, For, Show } from 'solid-js';
+import type { CellContext, ColumnDef } from '@tanstack/solid-table';
+import { createSignal, For, JSX, Show } from 'solid-js';
 import { z } from 'zod';
 import { makePersisted } from '@solid-primitives/storage';
 import { JobsResponse } from '~/lib/queries';
@@ -11,28 +11,61 @@ function truncate(value: string | undefined, maxLength: number) {
   return value && value.length > maxLength ? `${value.slice(0, maxLength)}...` : value || '';
 }
 
+function WithTooltip(props: {
+  info: CellContext<JobsResponse[0], unknown>;
+  renderWhenAgg: JSX.Element;
+  renderTooltipContent: JSX.Element;
+  fallback?: JSX.Element;
+  showCount?: boolean;
+  shouldShowContent?: boolean;
+}) {
+  return (
+    <Show
+      when={props.info.row.original.isAggregated}
+      fallback={props.fallback || props.info.getValue<string>()}
+    >
+      <Tooltip openDelay={300} gutter={0}>
+        <div class="inline-flex items-center gap-1">
+          <span>{props.renderWhenAgg}</span>
+          <Show when={props.info.row.original.count > 1 && props.shouldShowContent}>
+            <TooltipTrigger>
+              <span class="w-fit rounded-sm bg-stone-900 px-0.5 text-xs text-stone-100">
+                <Show when={props.showCount} fallback="+">
+                  {props.info.row.original.count}
+                </Show>
+              </span>
+            </TooltipTrigger>
+          </Show>
+        </div>
+        <Show when={props.shouldShowContent}>
+          <TooltipContent
+            class="flex max-h-40 flex-col gap-1 overflow-auto truncate text-xs"
+            style={{
+              'max-width': `${Math.max(props.info.column.getSize(), 250)}px`,
+            }}
+          >
+            {props.renderTooltipContent}
+          </TooltipContent>
+        </Show>
+      </Tooltip>
+    </Show>
+  );
+}
+
 export const defaultColumns = [
   {
     id: 'id',
     accessorKey: 'id',
     header: () => 'ID',
     cell: (info) => {
-      const isAggregated = info.row.original.isAggregated;
-      const count = info.row.original.count;
       return (
-        <Show when={isAggregated} fallback={info.getValue<string>()}>
-          <Tooltip gutter={0}>
-            <TooltipTrigger class="inline-flex items-center gap-1">
-              <span>aggregated</span>
-              <span class="w-fit rounded-sm bg-stone-900 px-0.5 text-xs text-stone-100">
-                {count}
-              </span>
-            </TooltipTrigger>
-            <TooltipContent class="flex max-h-40 flex-col gap-1 overflow-y-auto text-xs" as="ul">
-              <For each={info.row.original.ids}>{(id) => <li>{id}</li>}</For>
-            </TooltipContent>
-          </Tooltip>
-        </Show>
+        <WithTooltip
+          info={info}
+          renderWhenAgg={'aggregated'}
+          renderTooltipContent={<For each={info.row.original.ids}>{(id) => <span>{id}</span>}</For>}
+          showCount
+          shouldShowContent
+        />
       );
     },
     size: 140,
@@ -67,7 +100,18 @@ export const defaultColumns = [
     id: 'remote',
     accessorKey: 'remote',
     header: () => 'Remote',
-    cell: (info) => truncate(info.getValue<string[]>()?.at(0), 30),
+    cell: (info) => {
+      return (
+        <WithTooltip
+          info={info}
+          renderWhenAgg={truncate(info.getValue<string[]>()?.at(0), 30)}
+          renderTooltipContent={
+            <For each={info.getValue<string[]>().slice(1)}>{(l) => <span>{l}</span>}</For>
+          }
+          shouldShowContent={info.getValue<string[]>().length > 1}
+        />
+      );
+    },
     size: 80,
   },
   {
@@ -75,7 +119,19 @@ export const defaultColumns = [
     accessorKey: 'locations',
     header: () => 'Location',
     size: 250,
-    cell: (info) => truncate(info.getValue<string[]>()?.at(0), 30),
+    cell: (info) => {
+      return (
+        <WithTooltip
+          info={info}
+          renderWhenAgg={truncate(info.getValue<string[]>()?.at(0), 30)}
+          fallback={truncate(info.getValue<string[]>()?.at(0), 30)}
+          renderTooltipContent={
+            <For each={info.getValue<string[]>().slice(1)}>{(l) => <span>{l}</span>}</For>
+          }
+          shouldShowContent={info.getValue<string[]>().length > 1}
+        />
+      );
+    },
   },
   {
     id: 'timeSincePosted',
@@ -94,18 +150,34 @@ export const defaultColumns = [
     accessorKey: 'links',
     header: () => 'Link',
     cell: (info) => {
-      const validationResult = urlSchema.safeParse(info.getValue<string[]>()?.at(0));
+      const renderLink = (link?: string, showLink = false) => {
+        const validationResult = urlSchema.safeParse(link);
+        return (
+          <Show when={validationResult.success} fallback="-">
+            <a href={validationResult.data} target="_blank" rel="noopener noreferrer">
+              {showLink ? validationResult.data : 'Link'}
+            </a>
+          </Show>
+        );
+      };
+      const value = info.getValue<string[]>().at(0);
+
       return (
-        <Show when={validationResult.success} fallback="-">
-          <a href={validationResult.data} target="_blank" rel="noopener noreferrer">
-            Link
-          </a>
-        </Show>
+        <WithTooltip
+          info={info}
+          fallback={renderLink(value)}
+          renderWhenAgg={renderLink(value)}
+          renderTooltipContent={
+            <For each={info.getValue<string[]>()}>{(l) => <span>{renderLink(l, true)}</span>}</For>
+          }
+          shouldShowContent
+        />
       );
     },
-    size: 40,
+    size: 60,
   },
-] satisfies ColumnDef<JobsResponse[0]>[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+] satisfies ColumnDef<JobsResponse[0], any>[];
 
 export const [columnOrder, setColumnOrder] = makePersisted(
   // eslint-disable-next-line solid/reactivity
