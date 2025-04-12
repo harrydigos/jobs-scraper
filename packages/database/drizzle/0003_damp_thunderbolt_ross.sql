7e2e46a07e7ae6e01acd58c4dbaf398fd1eb15c8
Harry Digos <harrydigos@gmail.com>
2025-04-12 20:37:55 +0300

    feat(db): add company_id to db schema

---
+++ packages/database/drizzle/0003_damp_thunderbolt_ross.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `jobs` ADD `company_id` text;
---
+++ packages/database/drizzle/0004_optimal_tag.sql
@@ -0,0 +1,30 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_jobs` (
+	`id` text PRIMARY KEY NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`title` text NOT NULL,
+	`link` text NOT NULL,
+	`company` text NOT NULL,
+	`location` text NOT NULL,
+	`company_link` text NOT NULL,
+	`company_id` text NOT NULL,
+	`description` text,
+	`company_img_link` text,
+	`company_size` text,
+	`remote` text,
+	`is_promoted` integer,
+	`job_insights` text,
+	`time_since_posted` text,
+	`is_reposted` integer,
+	`skills_required` text,
+	`requirements` text,
+	`apply_link` text
+);
+--> statement-breakpoint
+INSERT INTO `__new_jobs`("id", "created_at", "updated_at", "title", "link", "company", "location", "company_link", "company_id", "description", "company_img_link", "company_size", "remote", "is_promoted", "job_insights", "time_since_posted", "is_reposted", "skills_required", "requirements", "apply_link") SELECT "id", "created_at", "updated_at", "title", "link", "company", "location", "company_link", "company_id", "description", "company_img_link", "company_size", "remote", "is_promoted", "job_insights", "time_since_posted", "is_reposted", "skills_required", "requirements", "apply_link" FROM `jobs`;--> statement-breakpoint
+DROP TABLE `jobs`;--> statement-breakpoint
+ALTER TABLE `__new_jobs` RENAME TO `jobs`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE INDEX `title_idx` ON `jobs` (`title`);--> statement-breakpoint
+CREATE INDEX `company_idx` ON `jobs` (`company`);
